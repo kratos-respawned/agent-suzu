a51e5c781d4fe2b0b0237f767940fff1d3d0809f
Gaurav <sbhandari1907@protonmail.com>
2025-05-11 01:09:31 +0530

    remove build script

--- index.ts
+++ index.ts
@@ -1,8 +1,8 @@
 import { env } from "./env";
 import { bot } from "./bot";
 import { db, deleteKeys } from "./redis";
-await deleteKeys(`user-messages`);
-await deleteKeys(`memory:*`);
+//await deleteKeys(`user-messages`);
+//await deleteKeys(`memory:*`);
 console.log("Starting bot");
 bot.start();
 
